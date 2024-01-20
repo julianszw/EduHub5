cafae3e8ba08bf8835f938d8b638fbcf5653af61
JSzwarcman <jszwarcman@gmail.com>
2024-01-20 18:37:23 -0300

    Creación de clase Period para generar cursadas semestrales

---
+++ src/main/java/modelClasses/Period.java
@@ -0,0 +1,29 @@
+package modelClasses;
+
+import java.time.Year;
+
+public class Period {
+    private int semester;
+    private Year year;
+
+    public Period(int semester, Year year) {
+        this.semester = semester;
+        this.year = year;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public void setYear(Year year) {
+        this.year = year;
+    }
+}
