7c10ed0374329f378493141e62be2ddbd3ef6635
JSzwarcman <jszwarcman@gmail.com>
2024-02-04 15:40:48 -0300

    Comentarios

--- src/main/java/model/classes/Employee.java
+++ src/main/java/model/classes/Employee.java
@@ -6,6 +6,8 @@ import enums.Gender;
 import java.time.LocalDate;
 
 public class Employee extends Person{
+	//TODO
+	// 	Role role
 
 	public Employee(String firstName, String lastName, Country nationality, String ID, Gender gender, String email, LocalDate birthDate) {
 		super(firstName, lastName, nationality, ID, gender, email, birthDate);
