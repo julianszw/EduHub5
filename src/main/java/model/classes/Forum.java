76b9cafe2f20083471414a8530e73872513a1599
JSzwarcman <jszwarcman@gmail.com>
2024-02-04 15:41:22 -0300

    Comentarios

--- src/main/java/model/classes/Forum.java
+++ src/main/java/model/classes/Forum.java
@@ -3,4 +3,6 @@ package model.classes;
 public class Forum {
   //TODO cada materia deberia tener su foro
   //     puede haber un foro común de la carrera, de la universidad, uno transversal a todas las cátedras
+
+//TODO hacer una app (API))
 }
