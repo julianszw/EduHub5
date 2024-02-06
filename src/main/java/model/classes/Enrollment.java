a5eba88711057eed53cbce766d4bf2f03bdeb1af
JSzwarcman <jszwarcman@gmail.com>
2024-02-06 17:22:18 -0300

    Constructor

--- src/main/java/model/classes/Enrollment.java
+++ src/main/java/model/classes/Enrollment.java
@@ -7,4 +7,21 @@ public class Enrollment {
     private DegreeProgram degreeProgram;
     private ArrayList<Course> finishedCourses; //accredited/ approved
 
+    public Enrollment(DegreeProgram degreeProgram, Student student) {
+        this.degreeProgram = degreeProgram;
+        this.student = student;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public DegreeProgram getDegreeProgram() {
+        return degreeProgram;
+    }
+
+    public ArrayList<Course> getFinishedCourses() {
+        //TODO vale la pena que todas las instancias tengan esta lista?
+        return finishedCourses;
+    }
 }
