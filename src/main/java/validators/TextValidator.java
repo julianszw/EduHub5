00f752b50b1ebb359dc70f2fca20f4ce670ca439
JSzwarcman <jszwarcman@gmail.com>
2024-03-11 18:48:26 -0300

    Simplificación de regex para nombres

--- src/main/java/validators/TextValidator.java
+++ src/main/java/validators/TextValidator.java
@@ -2,60 +2,57 @@ package validators;
 
 import enums.Country;
 
-public class TextValidator {
-	private final static String NAME_REGEX = "^[a-zA-Z'\\s]+$"; //TODO ¿Mismo Regex que para lastName?
-	private final static String LAST_NAME_REGEX = "^[a-zA-Z'\\-]+$"; //Se agregan apóstrofes y guiones //TODO Agregaría especios
-	private final static String PROGRAM_CODE_REGEX = "^[a-zA-Z]{3}$"; //TODO ¿hacer un GENERIC_CODE_REGEX?
-	private final static String COURSE_CODE_REGEX = "^[a-zA-Z]{3}$";
-	private final static String BRANCH_CODE_REGEX = "^[a-zA-Z]{3}$";
-	private final static String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
-	private final static String EMAIL_USER_REGEX = "^[a-zA-Z0-9._%+-]+$";
-	private final static String EMAIL_DOMAIN_REGEX = "^[a-zA-Z0-9.-]+$";
-	private final static String DEGREE_PROGRAM_NAME_REGEX = "^[A-Za-z\\s]{1,45}$";
-	private final static String COURSE_NAME_REGEX = "^[A-Za-z\\s]{1,45}$";
+import java.util.regex.Pattern;
 
+public class TextValidator {
+	private final static Pattern NAME_REGEX = Pattern.compile("^[a-zA-Z'\\- ]+$");
+	private final static Pattern PROGRAM_CODE_REGEX = Pattern.compile("^[a-zA-Z]{3}$");
+	private final static Pattern COURSE_CODE_REGEX = Pattern.compile("^[a-zA-Z]{3}$");
+	private final static Pattern BRANCH_CODE_REGEX = Pattern.compile("^[a-zA-Z]{3}$");
+	private final static Pattern EMAIL_REGEX = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+	private final static Pattern EMAIL_USER_REGEX = Pattern.compile("^[a-zA-Z0-9._%+-]+$");
+	private final static Pattern EMAIL_DOMAIN_REGEX = Pattern.compile("^[a-zA-Z0-9.-]+$");
+	private final static Pattern DEGREE_PROGRAM_NAME_REGEX = Pattern.compile("^[A-Za-z\\s]{1,45}$");
+	private final static Pattern COURSE_NAME_REGEX = Pattern.compile("^[A-Za-z\\s]{1,45}$");
 
 	public static boolean isValidName(String name) {
-		return name != null && name.matches(NAME_REGEX);
-	}
-
-	public static boolean isValidLastName(String lastName) {
-		return lastName != null && lastName.matches(LAST_NAME_REGEX);
+		return name != null && NAME_REGEX.matcher(name).matches();
 	}
 
 	public static boolean isValidEmail(String email) {
-		return email != null && email.matches(EMAIL_REGEX);
+		return email != null && EMAIL_REGEX.matcher(email).matches();
 	}
 
 	public static boolean isValidDegreeProgramCode(String programCode) {
-		return programCode != null && programCode.matches(PROGRAM_CODE_REGEX);
+		return programCode != null && PROGRAM_CODE_REGEX.matcher(programCode).matches();
 	}
 
 	public static boolean isValidCourseCode(String courseCode) {
-		return courseCode != null && courseCode.matches(COURSE_CODE_REGEX);
+		return courseCode != null && COURSE_CODE_REGEX.matcher(courseCode).matches();
 	}
 
 	public static boolean isValidBranchCode(String branchCode) {
-		return branchCode != null && branchCode.matches(BRANCH_CODE_REGEX);
+		return branchCode != null && BRANCH_CODE_REGEX.matcher(branchCode).matches();
 	}
 
 	public static boolean isValidID(String ID, Country country) {
-		return ID.matches(country.getIDPattern());
+		return ID != null && ID.matches(country.getIDPattern());
 	}
 
 	public static boolean isValidUserEmail(String emailUser) {
-		return emailUser.matches(EMAIL_USER_REGEX);
+		return emailUser != null && EMAIL_USER_REGEX.matcher(emailUser).matches();
 	}
 
 	public static boolean isValidDomainEmail(String emailDomain) {
-		return emailDomain.matches(EMAIL_DOMAIN_REGEX);
+		return emailDomain != null && EMAIL_DOMAIN_REGEX.matcher(emailDomain).matches();
 	}
 
 	public static boolean isValidDegreeProgramName(String programName) {
-		return programName.matches(DEGREE_PROGRAM_NAME_REGEX);
+		return programName != null && DEGREE_PROGRAM_NAME_REGEX.matcher(programName).matches();
 	}
 
 	public static boolean isValidCourseName(String courseName) {
-		return courseName.matches(COURSE_NAME_REGEX);
+		return courseName != null && COURSE_NAME_REGEX.matcher(courseName).matches();
 	}
+
 }
